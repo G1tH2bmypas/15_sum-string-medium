72c20010cbcab04428316db10a1118658a7af85a
Tekyto <Tekyto2014@gmail.com>
2022-10-19 01:34:03 +0300

    negative

--- index.cpp
+++ index.cpp
@@ -3,5 +3,25 @@
 #include <cmath>
 
 int sumString(const std::string& str) {
-	return 0;
+	int num = 0;
+	int negative = false;
+
+	for (char ch: str) {
+		if (ch == ',') {
+			continue;
+		}
+		if ( ch == '-') {
+			negative = true;
+			continue;
+		}
+
+		int current = ch - 48;
+
+		if (negative) {
+			current *= -1;
+			negative = false;
+		}
+		num += current;
+	}
+	return num;
 }
